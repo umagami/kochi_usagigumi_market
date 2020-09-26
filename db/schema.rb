0330c85b0b9dc49ba5d4a0e8a4fbb03389e86977
softtennis <softtennis.persona@gmail.com>
2020-09-26 19:20:10 +0900

    delete_migration_devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_26_025321) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
