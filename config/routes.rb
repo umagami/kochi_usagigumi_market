315e38b22f2bb79e2788f10b869d337a1a5ed257
softtennis <softtennis.persona@gmail.com>
2020-09-28 15:50:44 +0900

    mypage-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  # get 'items/buy/:id', to: 'items#buy', as: 'buy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
   resources :items, only: [:index, :show] do
     member do 
      get 'buy'
