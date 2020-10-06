3eb55245cc4902b47491a29e29712e5da79f943c
y-yamada0218 <69231228+y-yamada0218@users.noreply.github.com>
2020-10-06 13:04:43 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.valid?
       @item.save
-      redirect_to new_item_path
+      redirect_to root_path
     else 
       @item.item_images.new
       render :new and return
