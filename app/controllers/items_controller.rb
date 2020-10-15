949e7975fca22777dad0faec4160e81b842f970f
tmecco <takeyoshi0613@icloud.com>
2020-10-15 16:40:58 +0900

    アップデート

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.valid? && @item.save
+    if @item.save
       redirect_to root_path
     else
       @item.item_images.new
