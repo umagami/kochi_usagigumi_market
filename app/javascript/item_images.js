511970df83450599cdb80c0fd480f01f9d15ca14
y-yamada0218 <69231228+y-yamada0218@users.noreply.github.com>
2020-10-23 09:04:36 +0900

    update

--- app/javascript/item_images.js
+++ app/javascript/item_images.js
@@ -1,20 +1,33 @@
 $(window).on('load', ()=> {
   const buildFileField = (index)=> {
-    const html = `<div  class="js-file_group" data-index="${index}">
-                  <label class="label" for="item_item_images_attributes_${index}_image_url">
-                    <div class="image-icon">
-                      <i class="fas fa-camera fa-4x"></i>
-                    </div>
-                    <input class="js-file" type="file"
-                    name="item[item_images_attributes][${index}][image_url]"
+    if (index < 5) {
+    var html = `<div class="file_${index}" data-index="${index}">
+                    <input class="js-file" type="file" 
+                    name="item[item_images_attributes][${index}][image_url]" 
                     id="item_item_images_attributes_${index}_image_url">
-                    </label>
-                    <span class="js-remove">削除</span>
-                  </div>`;
+                  </div>
+                  <label class="image-label" for="item_item_images_attributes_${index}_image_url">
+                    <div class="image-icon">
+                    <i class="fas fa-camera icon"></i>
+                    </div>
+                  </label>`
+                  ;
+    } else {
+      var html = ``
+    }
     return html;
   }
+
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `
+                  <div class="content">
+                    <img data-index="${index}" src="${url}" width="100px" height="100px"><br>
+                    <div class="image-option" data-index="${index}">
+                      <span class="edit">編集</span>
+                      <span class="js-remove">削除</span>
+                    </div>
+                  </div>
+                  `;
     return html;
   }
 
@@ -32,18 +45,21 @@ $(window).on('load', ()=> {
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('image_url', blobUrl);
     } else {  
+      $(".image-label").remove();
       $('#previews').append(buildImg(targetIndex, blobUrl));
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+      console.log(fileIndex[fileIndex.length - 4]-1)
     }
   });
   $('#image-box').on('click', '.js-remove', function() {
     const targetIndex = $(this).parent().data('index')
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    $(this).parent().parent().remove();
+    $(`.file_${targetIndex}`).remove();
+    // $(`img[data-index="${targetIndex}"]`).remove();
+    if ($('.file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
