15d1904e3b255b85f6b562557153506c4932ad31
y-yamada0218 <69231228+y-yamada0218@users.noreply.github.com>
2020-10-26 17:58:46 +0900

    update

--- app/javascript/item_images.js
+++ app/javascript/item_images.js
@@ -1,13 +1,13 @@
 $(window).on('load', ()=> {
   const buildFileField = (index)=> {
-    if ($('li').length < 5) {
+    if ($('.content').length < 5) {
     var html = `
-                  <li class="file_${index + 1}" data-index="${index + 1}">
+                  <li class="file_${index}" data-index="${index}">
                     <input class="js-file" type="file" 
-                    name="item[item_images_attributes][${index + 1}][image_url]" 
-                    id="item_item_images_attributes_${index + 1}_image_url">
+                    name="item[item_images_attributes][${index}][image_url]" 
+                    id="item_item_images_attributes_${index}_image_url">
                   </li>
-                  <label class="image-label_${index + 1}" for="item_item_images_attributes_${index + 1}_image_url">
+                  <label class="image-label" for="item_item_images_attributes_${index}_image_url">
                     <div class="image-icon">
                     <i class="fas fa-camera icon"></i>
                     </div>
@@ -31,33 +31,42 @@ $(window).on('load', ()=> {
     return html;
   }
 
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+  console.log($('.content').length)
+  if ($('.content').length != 1) {
+    fileIndex.splice(0, $('.content').length);
+  }
+  console.log(fileIndex)
+
   $('.hidden-destroy').hide();
 
   $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
+
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('image_url', blobUrl);
     } else {  
-      $(`.image-label_${targetIndex}`).remove();
+      $(`.image-label`).remove();
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      $('#image-box').append(buildFileField(targetIndex));
+      $('#image-box').append(buildFileField(fileIndex[0] + 1));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length]);
     }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
     const targetIndex = $(this).parent().data('index')
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    console.log(targetIndex)
-    console.log(hiddenCheck)
     
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().parent().remove();
     $(`.file_${targetIndex}`).remove();
-    $(`.image-label_${targetIndex}`).remove();
-     if ($('li').length == 0 || $('li').length == 4) {
-     $('#image-box').append(buildFileField(targetIndex))
+     if ( $('.content').length == 4) {
+     $('#image-box').append(buildFileField(fileIndex[0]))
      };
   });
 });
