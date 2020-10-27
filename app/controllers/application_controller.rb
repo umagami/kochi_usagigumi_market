634cd2cc03a1f90b2216ca2919346069e3e16e68
y-yamada0218 <69231228+y-yamada0218@users.noreply.github.com>
2020-10-27 13:37:33 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # before_action :basic_auth, if: :production?
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
   
 
   protected
@@ -12,12 +13,19 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == Rails.application.credentials[:basic_auth][:user] &&
+  #     password == Rails.application.credentials[:basic_auth][:pass]
+  #   end
+  # end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
+      username == 'usagi' && password == 'usagigumi'
     end
   end
+  
 
   def production?
     Rails.env.production?
